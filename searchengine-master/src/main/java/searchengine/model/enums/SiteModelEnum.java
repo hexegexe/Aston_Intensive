c443799f1b97d918bf2a9212e8ccafd3a47c8f02
hexegexe <150177677+hexegexe@users.noreply.github.com>
2023-12-15 12:37:03 +0500

    Revert "add project for CR"

--- searchengine-master/src/main/java/searchengine/model/enums/SiteModelEnum.java
+++
@@ -1,7 +0,0 @@
-package searchengine.model.enums;
-
-public enum SiteModelEnum {
-    INDEXING,
-    INDEXED,
-    FAILED
-}
--- searchengine-master/src/main/java/searchengine/services/SearchService.java
+++
@@ -1,8 +0,0 @@
-package searchengine.services;
-
-import searchengine.dto.search.SearchResponse;
-
-public interface SearchService {
-
-    SearchResponse getResultsPagesForSearch(String query, String site, Integer offset, Integer limit);
-}
